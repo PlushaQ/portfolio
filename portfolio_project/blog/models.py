5e89d530850fa289777d678e1f794f2835389da6
Maciej Fularz <fularzmaciej@gmail.com>
2022-09-08 21:24:13 +0200

    added blog app

--- portfolio_project/blog/models.py
+++ portfolio_project/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField(max_length=5000)
+    image = models.ImageField(upload_to='images/')
+    publish_date = models.DateTimeField()
